af3006d5a7917ee85e59a3994338051f1c7c41ea
dimas <dimas.maulana370@gmail.com>
2022-12-09 13:35:42 +0700

    form crud

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"gin-mongo-api/configs"
+	"gin-mongo-api/routes" //add this
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.UserRoute(router)         //add this
+	routes.InvertebrataRoute(router) //add this
+	routes.VertebrataRoute(router)   //add this
+	routes.FosilRoute(router)        //add this
+
+	router.Run("localhost:8080")
+}
